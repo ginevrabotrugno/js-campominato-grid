22a95db5e71dd9a43b527c9eafc919b55e0f22a1
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-13 16:42:24 +0200

    feat: add base structure

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
+
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
+// seleziono bottone dall html 
+// gestisco l'interazione al click
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // seleziono il contenitore dei blocchi
+// const grid = document.getElementById("grid");
+
+// // per 64 volte faccio qualcosa
+// for (let i = 0; i < 64; i++) {
+
+//     const box = createElementWithClass("div", "square");
+//     console.log(box);
+
+//     // gestire il click sul singolo elemento
+//     box.addEventListener("click",
+
+//         function () {
+//             this.classList.toggle("clicked");
+//         }
+//     )
+
+//     grid.append(box);
+
+// }
